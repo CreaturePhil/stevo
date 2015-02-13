5798e47dc2f20ba4dd1adaed955aba73f8480eff
Phil <birkal@outlook.com>
2015-02-12 21:02:51 -0800

    Add commands options

--- test.js
+++ test.js
@@ -2,5 +2,10 @@ require('./index').createServer('PokemonShowdown', {
   owner: 'CreaturePhil',
   config: {
     port: 3000
+  },
+  commands: {
+    stevo: function(target, room, user, connection, cmd) {
+      this.sendReply('yo');
+    }
   }
 });
